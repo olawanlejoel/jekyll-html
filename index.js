3c80dd7c23473fa01d38beeab81a6832a4d96baa
Marcia Ramos <13464393+VirtuaCreative@users.noreply.github.com>
2023-09-12 12:50:56 +0100

    Fix index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 // const port = 3000;
 
-app.use(express.static('_site'));
+app.use('/_site', express.static('_site'));
 
 app.get("/", (req, res) => {
     res.sendFile('_site' + "/index.html");
