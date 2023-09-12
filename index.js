66482b8cfb112ae4df71d1cb5790144b6640308d
Marcia Ramos <13464393+VirtuaCreative@users.noreply.github.com>
2023-09-12 13:29:55 +0100

    Fix path (4)

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 const express = require("express");
 const app = express();
-// const port = 3000;
-const path = require('path');
 
-import {fileURLToPath} from 'url';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-console.log(path.join(__dirname, 'index.html'));
-
-app.use(express.static(__dirname + '/_site'));
+app.use(express.static(__dirname + './_site'));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, './_site/index.html'));
+    res.sendFile(__dirname + "./_site/index.html");
 });
 
 // app.listen(port, () => {
