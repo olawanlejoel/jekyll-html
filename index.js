03842c8214be1bfd44fe2671d748a1ded89a0f71
Marcia Ramos <13464393+VirtuaCreative@users.noreply.github.com>
2023-09-12 13:38:37 +0100

    Fix path (7)

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static(__dirname + './_site'));
 
 app.get("/", (req, res) => {
-    res.sendFile('_site/index.html' , { root : __dirname});
+    res.sendFile( __dirname + "/_site/" + "index.html" );
 });
 
 // app.listen(port, () => {
