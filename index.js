4ed4c1fd37f7a21094309e474d125ab07d3a22ef
Marcia Ramos <13464393+VirtuaCreative@users.noreply.github.com>
2023-09-12 13:26:44 +0100

    Fix path (3)

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const path = require('path');
 
 import {fileURLToPath} from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 console.log(path.join(__dirname, 'index.html'));
 
