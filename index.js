efdf771a8b49b93127a88ba1d5cdabf1b07c6270
Marcia Ramos <13464393+VirtuaCreative@users.noreply.github.com>
2023-09-12 14:07:15 +0100

    Fix path (12)

--- index.js
+++ index.js
@@ -2,14 +2,10 @@ const express = require("express");
 const app = express();
 // const port = 3000;
 
-var path = require('path');
-var filename = path.basename(__filename);
-console.log(filename);
-
 app.use(express.static(__dirname + '/_site'));
 
 app.get("/", (req, res) => {
-    res.sendFile('./_site/index.html', { root: __dirname });
+    res.sendFile('/_site/index.html', { root: __dirname });
 });
 
 // app.listen(port, () => {
