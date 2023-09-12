65b6a615686f3ac5b7a00bb1b6bb4f44ad74df6d
Marcia Ramos <13464393+VirtuaCreative@users.noreply.github.com>
2023-09-12 13:46:52 +0100

    Remove listening port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+// const port = 3000;
 
 app.use(express.static(__dirname + '/_site'));
 
@@ -8,6 +8,6 @@ app.get("/", (req, res) => {
     res.sendFile( __dirname + "/_site/" + "index.html" );
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
-});
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}!`);
+// });
