2e5f6fa2c4f765af7d0e9c557cf068d6f24aa6c9
Marcia Ramos <13464393+VirtuaCreative@users.noreply.github.com>
2023-09-12 12:40:01 +0100

    Add Jekyll's complete codebase

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-// const port = 443;
+// const port = 3000;
 
-app.use(express.static(__dirname));
+app.use(express.static('_site'));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile('_site' + "/index.html");
 });
 
 // app.listen(port, () => {
