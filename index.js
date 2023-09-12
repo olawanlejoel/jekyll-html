b56b55fd252c1dc1bd9169d158aee497c98b4a4c
Marcia Ramos <13464393+VirtuaCreative@users.noreply.github.com>
2023-09-12 14:33:55 +0100

    Try again

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-// const port = 443;
+const port = 3000;
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/_site'));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/_site/index.html");
 });
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}!`);
-// });
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
