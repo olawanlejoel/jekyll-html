2a60df0c65e0ef905da0a6509d051213a16519ca
Joel Olawanle <57611810+olawanlejoel@users.noreply.github.com>
2023-09-13 11:41:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.use(express.static(__dirname));
 
-app.listen(8080, () => {
-	console.log(`Jekyll Static Site listening on port 8080!`);
-});
+// app.listen(8080, () => {
+// 	console.log(`Jekyll Static Site listening on port 8080!`);
+// });
