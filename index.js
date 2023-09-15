fe07b301a3b917df10885ce641092bd063466cb4
Joel Olawanle <57611810+olawanlejoel@users.noreply.github.com>
2023-09-15 11:29:32 +0100

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static(__dirname));
-
-// app.listen(8080, () => {
-// 	console.log(`Jekyll Static Site listening on port 8080!`);
-// });
